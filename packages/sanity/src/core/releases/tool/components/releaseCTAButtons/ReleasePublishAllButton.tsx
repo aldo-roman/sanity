73a7824df369f9436679fe99e57bd107ba4feda9
Rita <anarita151@gmail.com>
2025-03-11 09:34:27 +0100

    refactor(core): add error messages for when no documents are releases and publish / schedule is attempted

--- packages/sanity/src/core/releases/tool/components/releaseCTAButtons/ReleasePublishAllButton.tsx
+++ packages/sanity/src/core/releases/tool/components/releaseCTAButtons/ReleasePublishAllButton.tsx
@@ -159,6 +159,10 @@ export const ReleasePublishAllButton = ({
     if (!hasDocumentValidationErrors && !isValidatingDocuments && publishPermission) return null
 
     const tooltipText = () => {
+      if (documents.length === 0) {
+        return t('publish-action.validation.no-documents')
+      }
+
       if (!publishPermission) {
         return t('publish-dialog.validation.no-permission')
       }
@@ -183,7 +187,7 @@ export const ReleasePublishAllButton = ({
         </Flex>
       </Text>
     )
-  }, [hasDocumentValidationErrors, isValidatingDocuments, publishPermission, t])
+  }, [documents.length, hasDocumentValidationErrors, isValidatingDocuments, publishPermission, t])
 
   return (
     <>
@@ -196,7 +200,11 @@ export const ReleasePublishAllButton = ({
           }}
           tone="primary"
           icon={PublishIcon}
-          disabled={isPublishButtonDisabled || publishBundleStatus === 'publishing'}
+          disabled={
+            isPublishButtonDisabled ||
+            publishBundleStatus === 'publishing' ||
+            documents.length === 0
+          }
           text={t('action.publish-all-documents')}
           onClick={() => setPublishBundleStatus('confirm')}
           data-testid="schedule-button"
@@ -209,7 +217,11 @@ export const ReleasePublishAllButton = ({
             placement: 'bottom',
           }}
           icon={PublishIcon}
-          disabled={isPublishButtonDisabled || publishBundleStatus === 'publishing'}
+          disabled={
+            isPublishButtonDisabled ||
+            publishBundleStatus === 'publishing' ||
+            documents.length === 0
+          }
           text={t('action.publish-all-documents')}
           onClick={() => setPublishBundleStatus('confirm')}
           loading={publishBundleStatus === 'publishing'}
